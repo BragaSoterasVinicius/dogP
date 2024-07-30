1d00979f2cac3dda3212c91e3f484515712f4f2e
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-07-30 12:42:22 -0300

    primeiro commit
    É estranhamente cansativo ter que estabelecer uma nova pasta com as configurações certas toda vez que perco a organização na minha cabeça.

---
+++ src/main/java/com/soter/dogp/DogpApplication.java
@@ -0,0 +1,13 @@
+package com.soter.dogp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soter/dogp/DogpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soter.dogp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
