ed0c4c32f93050d9edf2a1cc3594e4c8146d4034
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-07-31 21:09:21 -0300

    permanencia de sessão
    vai ser importante no login

--- src/main/java/com/soter/dogp/controller/TestController.java
+++ src/main/java/com/soter/dogp/controller/TestController.java
@@ -4,12 +4,15 @@ import com.soter.dogp.objcts.Posts;
 import com.soter.dogp.objcts.User;
 import com.soter.dogp.repo.PostRepo;
 import com.soter.dogp.repo.UserRepo;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+//aqui vai ser a nossa classezinha de debogagem, vai ser apagado em prod confia
+@RestController
 public class TestController {
 
     private PostRepo postRepo;
@@ -19,12 +22,20 @@ public class TestController {
         this.postRepo = postRepo;
         this.userRepo = userRepo;
     }
+    private final String HOME_VIEW_COUNT = "HOME_VIEW_COUNT";
+    @GetMapping("/")
+    public String postRepoTeste(HttpSession session){
+        incrementCount(session, HOME_VIEW_COUNT);
+        return "testeDiferente, " + session.getAttribute(HOME_VIEW_COUNT);
+    }
 
-    @GetMapping("/repoTest")
-    public String postRepoTeste(){
+    @GetMapping("/count")
+    public String count(HttpSession session){
+        return "HOME_VIEW_COUNT: " +session.getAttribute(HOME_VIEW_COUNT);
+    }
 
-        User alberto = userRepo.getUserDataByUserId(4);
-        System.out.println(alberto.getNome());
-        return "teste";
+    private void incrementCount(HttpSession session, String attr) {
+        var homeViewCount = session.getAttribute(attr) == null ? 0 : (Integer) session.getAttribute(attr);
+        session.setAttribute(attr, homeViewCount += 1);
     }
 }
