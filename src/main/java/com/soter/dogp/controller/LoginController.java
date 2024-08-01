b32a534fc82067061003ea5be6eb3d508c07258b
BragaSoterasVinicius <viniciussoteras@gmail.com>
2024-07-31 22:40:01 -0300

    controller de login adicionado, bug encontrado
    olha, aparentemente o método encode do BcryptPasswordEncoder não faz o mesmo código para a mesma string. Vou procurar outro método ou biblioteca

--- src/main/java/com/soter/dogp/controller/LoginController.java
+++ src/main/java/com/soter/dogp/controller/LoginController.java
@@ -1,4 +1,30 @@
 package com.soter.dogp.controller;
 
+import com.soter.dogp.objcts.User;
+import com.soter.dogp.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
 public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginPage(Model model, @ModelAttribute("User") User userLogin){
+        return "login";
+    }
+    @GetMapping("/logar")
+    public String login(Model model, @ModelAttribute("User") User userLogin){
+        model.addAttribute("loginModel", new User());
+        String email = userLogin.getEmail();
+        String senha = userLogin.getSenha();
+        boolean logincerto = loginService.auth(email, senha);
+        System.out.print(logincerto);
+        return "login";
+    }
 }
--- src/main/java/com/soter/dogp/service/LoginService.java
+++ src/main/java/com/soter/dogp/service/LoginService.java
@@ -1,20 +1,20 @@
 package com.soter.dogp.service;
 
 import com.soter.dogp.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 
+@Service
 public class LoginService {
+    @Autowired
     private UserRepo userRepo;
     public boolean auth(String email, String senha){
         BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
         String encodedSenha = bcrypt.encode(senha);
         String senhaOnDb = userRepo.searchForUserSenha(email);
-        if (Objects.equals(encodedSenha, senhaOnDb)){
-            return true;
-        }else{
-            return false;
-        }
+        return Objects.equals(encodedSenha, senhaOnDb);
     }
 }
